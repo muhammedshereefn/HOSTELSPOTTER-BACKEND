6a8f4c65100c5fa84b102b162d73d65910dc7f30
unknown <mohdshereefn@gmail.com>
2024-08-15 10:18:43 +0530

    bug fix completed

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://hostelspotter-frontend.vercel.app',
     methods: ['GET', 'POST' , 'PUT' , 'DELETE'],
   },
 });
@@ -31,7 +31,7 @@ if (!MONGO_URI) {
 
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://hostelspotter-frontend.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   credentials: true,
 }));
